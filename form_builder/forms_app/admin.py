ae1c9e0ac135086c45d3e1869a5fd3e5c0ba6c16
rohan <rohansureshgupta@gmail.com>
2024-12-28 21:05:32 +0530

    Revert "changes in schema"
    This reverts commit f89db4a06f3bc70a6987685cd703470de6feecd5.

--- form_builder/forms_app/admin.py
+++ form_builder/forms_app/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import (Choices,Question,Form)
 
 # Register your models here.
-admin.site.register(Choices)
-admin.site.register(Question)
-admin.site.register(Form)
