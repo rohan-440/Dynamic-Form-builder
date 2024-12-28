f89db4a06f3bc70a6987685cd703470de6feecd5
rohan <rohansureshgupta@gmail.com>
2024-12-28 21:05:19 +0530

    changes in schema

--- form_builder/forms_app/admin.py
+++ form_builder/forms_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import (Choices,Question,Form)
 
 # Register your models here.
+admin.site.register(Choices)
+admin.site.register(Question)
+admin.site.register(Form)
--- form_builder/forms_app/models.py
+++ form_builder/forms_app/models.py
@@ -2,27 +2,45 @@ from django.db import models
 from .choices import QUESTION_CHOICES
 from django.contrib.auth.models import User
 # Create your models here.
+
+
+
 class BaseModel(models.Model):
-    createdAt : models.DateField(auto_now_add=True)
-    updatedAt : models.DateField(auto_now=True)
+    createdAt = models.DateField(auto_now_add=True)
+    updatedAt = models.DateField(auto_now=True)
     
     class Meta:
         abstract = True
+
+
+
+
         
 class Choices(BaseModel):
     choice = models.CharField(max_length=200)
+    def __str__(self):
+        return self.choice
+    
+    
+ 
+ 
     
 class Question(BaseModel):
-    question : models.CharField(max_length=200)
+    question =  models.CharField(max_length=200)
     question_type = models.CharField(max_length=200,choices=QUESTION_CHOICES)
     required = models.BooleanField(default=True)
     choices = models.ManyToManyField(Choices,related_name="question_choices",blank=True)
+    def __str__(self):
+        return self.question
+    
 
 class Form(BaseModel):
     code = models.CharField(max_length=200,unique=True)
     title = models.CharField(max_length=200)
     creator = models.ForeignKey(User,on_delete=models.CASCADE)
     questions = models.ManyToManyField(Question,related_name="questions")
+    def __str__(self):
+        return self.title
     
     
     
