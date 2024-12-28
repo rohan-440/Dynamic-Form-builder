b4aa9ff693de60905fdcd8d23d4f06cec1f1ebd9
rohan <rohansureshgupta@gmail.com>
2024-12-28 22:26:06 +0530

    repaired formAPI,fixed serializer of formAPI

--- form_builder/forms_app/serializers.py
+++ form_builder/forms_app/serializers.py
@@ -10,4 +10,29 @@ class QuestionSerializer(serializers.ModelSerializer):
 class FormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Form
-        exclude = ["createdAt","updatedAt"]
+        exclude = ["createdAt","updatedAt","creator","id"]
+    
+    def to_representation(self, instance):
+        questions = []
+        
+        for question in instance.questions.all():
+            choices = None
+            if question.question_type in ["Dropdown","Checkbox"]:
+                choices = []
+                for choice in question.choices.all():
+                    choices.append({"id" : choice.id, "choice" : choice.choice})
+            questions.append({
+                "question" : question.question,
+                "question_type"  :question.question_type,
+                "required": question.required,
+                "choices" : choices
+            })
+            
+            data = {
+                "id" : question.id,
+                "code" : instance.code,
+                "title" : instance.title,
+                "questions" : questions,
+            }
+            
+        return data
